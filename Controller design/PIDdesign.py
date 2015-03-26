cfd55560c3142abb1a4fa88ca530c09819316220
Maayan4 <maayan4temp@gmail.com>
2015-03-27 01:07:43 +0200

    ץ

--- Controller design/PIDdesign.py
+++ Controller design/PIDdesign.py
@@ -1,14 +1,22 @@
 __author__ = 'raziele'
 
+## Analyze motor measurement and determine the transfer function
+## Each file contains several system measurements.
+## This file organize the file an estimate the transfer function with kalman-filter
+
 import cmath, io, datetime, math
 import numpy as np
 import pylab
+import scipy.io as IO
 
-fname = '/Users/raziele/Reps/Targuino/RawData/log2015.dat'
+fname = '/Users/raziele/Reps/Targuino/RawData/logR2_MA2_lastVal_updated.dat'
 fmode = 'U'
 
 TEXT_SPACE = "\t"
 END_OF_LINE = "\r"
+
+rope_radius = 2.23 #[cm]
+
 
 time = np.array([])
 PH = np.array([])
@@ -33,39 +41,68 @@ with open(fname,fmode) as fid:
         DT = np.r_[DT,float(rowTmp[4])]
         LV = np.r_[LV,float(rowTmp[5])]
         AV = np.r_[AV,float(rowTmp[6])]
-        CU = np.r_[CU,float(rowTmp[7])]
+        CU = np.r_[CU,((float(rowTmp[7])-2.5)/0.066)]
         FA = np.r_[FA,float(rowTmp[8])]
         FB = np.r_[FB,float(rowTmp[9])]
         PS = np.r_[PS,float(rowTmp[10])]
 
+ax = pylab.figure(1)
+ax1 = pylab.subplot(2,4,1)
+pylab.plot(time,PH,'k+')
+pylab.xlabel('time')
+pylab.ylabel('$PH$')
+ax2 = pylab.subplot(2,4,2)
+pylab.plot(time,DT,'k+')
+pylab.xlabel('time')
+pylab.ylabel('$DT$')
+ax3 = pylab.subplot(2,4,3)
+pylab.plot(time,LV,'k+')
+pylab.xlabel('time')
+pylab.ylabel('$LV$')
+ax4 = pylab.subplot(2,4,4)
+pylab.plot(time,AV,'k+')
+pylab.xlabel('time')
+pylab.ylabel('$AV$')
+ax5 = pylab.subplot(2,4,5)
+pylab.plot(time,CU,'k+')
+pylab.xlabel('time')
+pylab.ylabel('$Current$')
+ax6 = pylab.subplot(2,4,6)
+pylab.plot(time,PS,'k+')
+pylab.xlabel('time')
+pylab.ylabel('$Power Supply$')
+ax7 = pylab.subplot(2,4,7)
+pylab.plot(PH,LV,'k+')
+pylab.xlabel('PH')
+pylab.ylabel('$LV$')
+ax8 = pylab.subplot(2,4,8)
+pylab.plot(PH, CU,'k+')
+pylab.xlabel('PH')
+pylab.ylabel('$Current$')
+
+#converting input value 0-254 to Linear velocity
+#input = ( PH / PH.max() ) *
 
 ind990=np.where(AV!=990)
-# check no-speed-measurement accumulation
+
+## check no-speed-measurement accumulation
 AV990=AV
 AV990[ind990]=0
 AV990=AV990.cumsum()
 AV990=AV990/AV990.max()
-pylab.figure()
-pylab.plot(AV990,'k+')
-
-print("\ncleaning...\n")
-zeroVal=np.where(AV==990)
-time = np.delete(time,zeroVal)
-PL = np.delete(PL,zeroVal)
-PH = np.delete(PH,zeroVal)
-R = np.delete(R,zeroVal)
-DT = np.delete(DT,zeroVal)
-LV = np.delete(LV,zeroVal)
-AV = np.delete(AV,zeroVal)
-CU = np.delete(CU,zeroVal)
-FA = np.delete(FA,zeroVal)
-FB = np.delete(FB,zeroVal)
-PS = np.delete(PS,zeroVal)
-
-pylab.figure()
-pylab.plot(PH, CU)
-pylab.figure()
-pylab.plot(PH, PS)
+# print("\ncleaning...\n")
+# zeroVal=np.where(AV==990)
+# time = np.delete(time,zeroVal)
+# PL = np.delete(PL,zeroVal)
+# PH = np.delete(PH,zeroVal)
+# R = np.delete(R,zeroVal)
+# DT = np.delete(DT,zeroVal)
+# LV = np.delete(LV,zeroVal)
+# AV = np.delete(AV,zeroVal)
+# CU = np.delete(CU,zeroVal)
+# FA = np.delete(FA,zeroVal)
+# FB = np.delete(FB,zeroVal)
+# PS = np.delete(PS,zeroVal)
 
 # Kalman filter
 
@@ -99,7 +136,7 @@ for k in range(1,n_iter):
     xhat[k] = xhatminus[k]+K[k]*(z[k]-xhatminus[k])
     P[k] = (1-K[k])*Pminus[k]
 
-pylab.figure()
+pylab.figure(4)
 pylab.plot(z,'k+',label='noisy measurements')
 pylab.plot(xhat,'b-',label='a posteri estimate')
 pylab.plot(PH/PH.max(), 'k--', label='input')
@@ -108,14 +145,20 @@ pylab.legend()
 pylab.xlabel('time')
 pylab.ylabel('Velocity')
 
-pylab.figure()
-valid_iter = range(1,n_iter) # Pminus not valid at step 0
-pylab.plot(valid_iter,Pminus[valid_iter],label='a priori error estimate')
-pylab.xlabel('Iteration')
-pylab.ylabel('$(Velocity)^2$')
-pylab.setp(pylab.gca(),'ylim',[0,.01])
-pylab.show()
+#pylab.figure(5)
+#valid_iter = range(1,n_iter) # Pminus not valid at step 0
+#pylab.plot(valid_iter,Pminus[valid_iter],label='a priori error estimate')
+#pylab.xlabel('Iteration')
+#pylab.ylabel('$(Velocity)^2$')
+#pylab.setp(pylab.gca(),'ylim',[0,.01])
 
 val=np.polyfit(PH,xhat,4,full=True)
+
+data={"xhat":xhat, "time":time, "PH":PH}
+
+IO.savemat("/Users/raziele/Reps/Targuino/R2_MA2_lastval.mat",data)
+
 print("finish")
+
+pylab.show()
 
