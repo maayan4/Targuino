2b0d4e7980a8ff60e5704b94fd36774c19010df8
root <root@localhost.localdomain>
2015-04-16 09:46:24 +0300

    .

--- Xbee/XbeeTest/XbeeTest.ino
+++ Xbee/XbeeTest/XbeeTest.ino
@@ -2,15 +2,18 @@
 const boolean DEBUG = 1;
 
 //Pins
-#define ledPin A2
-#define pot A0
+#define ledPin 19
+#define pot A2
 #define MSpin 14 //pin to determine whether the unit is in master or slave mode
 
 boolean MS=0; //default status 0 - arduino in slave mode (1 for master)
 int brightness = 0;    // how bright the LED is
 int fadeAmount = 3;    // how many points to fade the LED by
-int time = 1000;
+int DS = 150;
 
+//XBEE
+#define XBEE_DATA_RATE 57600
+String strIn(100);
 
 void setup() {
   Serial.begin(57600);
@@ -24,8 +27,8 @@ void setup() {
 void loop() {
 if(MS==0){ //SLAVE MODE
   if (Serial.available() > 0) {
-   time = Serial.parseInt();
-   Serial2.println(time);
+   DS = Serial.parseInt()*255/1023;
+   Serial2.println(DS);
   }
 }
 Serial2.println(Serial.available());
@@ -35,12 +38,29 @@ if(MS == 1){
   Serial2.println(analogRead(pot));
 }
 
-  analogWrite(ledPin, brightness);
+  digitalWrite(ledPin, DS);
 
   // change the brightness for next time through the loop:
-  brightness = 255 - brightness;
 
-  delay(time);
 }
 
+boolean beginXbee(){
+  Serial.begin(XBEE_DATA_RATE);
+  Serial.print("+++");
+}
 
+void serialEvent() {
+  while (Serial.available()) {
+    // get the new byte:
+    char inChar = (char)Serial.read();
+    // add it to the inputString:
+    strIn += inChar;
+    // if the incoming character is a newline, set a flag
+    // so the main loop can do something about it:
+    if (inChar == '\n\r') {
+      stringComplete = true;
+    }
+  }
+}
+ 
+ 
